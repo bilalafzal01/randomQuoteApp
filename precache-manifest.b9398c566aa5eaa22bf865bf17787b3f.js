e09062cb979a5d59eb8d45295c4bd0f1e5180862
Bilal Afzal <mafzal.bscs18seecs@seecs.edu.pk>
2020-06-23 01:11:19 +0500

    Updates

---
+++ precache-manifest.b9398c566aa5eaa22bf865bf17787b3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d87182ceab01bb9ba209c94483bf902",
+    "url": "/randomQuoteApp/index.html"
+  },
+  {
+    "revision": "2f6416d4490715f9cdcc",
+    "url": "/randomQuoteApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "822618238a669276fb39",
+    "url": "/randomQuoteApp/static/js/2.3df7e316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomQuoteApp/static/js/2.3df7e316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6416d4490715f9cdcc",
+    "url": "/randomQuoteApp/static/js/main.2524e3ff.chunk.js"
+  },
+  {
+    "revision": "87382afb317283b6d28b",
+    "url": "/randomQuoteApp/static/js/runtime-main.72cb5798.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/randomQuoteApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
